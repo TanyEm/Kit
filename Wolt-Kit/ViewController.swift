be0db0c033bcccc9c9703138f61d0808fc38dc73
TanyEm <tanyem@protonmail.com>
2021-01-04 22:48:17 +0200

    Initial Commit

---
+++ Wolt-Kit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wolt-Kit
+//
+//  Created by Tatiana Podlesnykh on 4.1.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
